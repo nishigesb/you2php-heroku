acedafd87c5e12e143f044a0832a8af17c76119e
nishigesb <47159229+nishigesb@users.noreply.github.com>
2019-01-30 00:24:32 +0100

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
-define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('APIKEY', 'AIzaSyAwXuMblO68tRm9xmiByfFwIjdAxHw00r4');//这里改成您的api key
+define('GJ_CODE', 'UK');//国家代码
+define('SITE_NAME', '某科学的视频网站');//网站名字
+define('TITLENAME', '某科学的视频网站');//网站名字
+define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
+define('EMAIL', '123@123.com');//邮箱
 ?>
